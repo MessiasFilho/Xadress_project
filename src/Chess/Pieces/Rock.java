66f17175ee49054d4de50f1a51173d4285a87068
MessiasFilho <messiasfilho98@gmail.com>
2022-04-19 17:23:02 -0300

    Create Rock.java

---
+++ src/Chess/Pieces/Rock.java
@@ -0,0 +1,20 @@
+package Chess.Pieces;
+
+import BoardGame.Board;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class Rock extends ChessPiece {
+
+	public Rock(Board board, Color color) {
+		super(board, color);
+	
+		
+		
+	}
+	@Override
+	public String toString() {
+			
+		return "R";
+	}
+}
