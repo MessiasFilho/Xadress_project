261abde610abda62769351e56d0b6219765b0c9b
MessiasFilho <messiasfilho98@gmail.com>
2022-04-19 19:24:06 -0300

    Create BoardExp.java

---
+++ src/BoardGame/BoardExp.java
@@ -0,0 +1,13 @@
+package BoardGame;
+
+public class BoardExp extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExp (String msg) {
+		super (msg); 
+	}
+	
+
+}
