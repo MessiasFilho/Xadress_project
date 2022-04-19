434a483d3b6ff9f5eaf52af9628d2a3431ca991d
MessiasFilho <messiasfilho98@gmail.com>
2022-04-19 15:28:23 -0300

    Update Position.java

--- src/BoardGame/Position.java
+++ src/BoardGame/Position.java
@@ -1,5 +1,42 @@
 package BoardGame;
 
 public class Position {
+	
+	private int Row ; 
+	private int Colun ;
+	
+	
+	public Position(int row, int colun) {
+		super();
+		Row = row;
+		Colun = colun;
+	}
+
+
+	public int getRow() {
+		return Row;
+	}
+
+
+	public void setRow(int row) {
+		Row = row;
+	}
+
+
+	public int getColun() {
+		return Colun;
+	}
+
+
+	public void setColun(int colun) {
+		Colun = colun;
+	} 
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return Row + "" + Colun;
+}
 
 }
