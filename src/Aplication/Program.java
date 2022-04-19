4cf1be84d809d94467a402772ce0657900659bdd
MessiasFilho <messiasfilho98@gmail.com>
2022-04-19 16:48:44 -0300

    Update Program.java

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,10 +1,16 @@
 package Aplication;
 
+import Chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+		ChessMatch mat = new ChessMatch(); 
+		
+		UI.printBoard(mat.getPieces()); 
 	}
 
 }
