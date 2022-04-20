a1444a0feffcdc1e463213e210889bb1c3057cb9
MessiasFilho <messiasfilho98@gmail.com>
2022-04-20 16:52:51 -0300

    Create ChessExp.java

---
+++ src/Chess/ChessExp.java
@@ -0,0 +1,11 @@
+package Chess;
+
+public class ChessExp extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExp (String msg) {
+		super(msg ) ; 
+	}
+
+}
