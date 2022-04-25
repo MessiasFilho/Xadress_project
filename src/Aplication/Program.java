4f5b82a38a7a31390875703d1ca6dbeac46185ea
MessiasFilho <messiasfilho98@gmail.com>
2022-04-25 15:53:25 -0300

    Update Program.java

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,16 +1,38 @@
 package Aplication;
 
+import java.util.Scanner;
+
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		Scanner sc =new Scanner (System.in); 
 		ChessMatch mat = new ChessMatch(); 
 		
-		UI.printBoard(mat.getPieces()); 
+		while (true) {
+			UI.printBoard(mat.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.ReadChessPosition(sc);
+			
+			System.out.println();
+			
+			System.out.print("Tareget: ");
+			
+			ChessPosition target = UI.ReadChessPosition(sc); 
+			
+				ChessPiece capturedPiece = mat.performChessMove(source, target); 
+			
+			
+		}
+		 
+		
+		
 	}
 
 }
