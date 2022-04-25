66b1ce222e148a6865537b54718e14ed97eed6f6
MessiasFilho <messiasfilho98@gmail.com>
2022-04-25 18:44:55 -0300

    Update King.java

--- src/Chess/Pieces/King.java
+++ src/Chess/Pieces/King.java
@@ -16,4 +16,11 @@ public class King extends ChessPiece{
 		// TODO Auto-generated method stub
 		return "K";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		
+		boolean [][]  mat = new boolean [getBoard().getRows()][getBoard().getColumns()] ; 
+		return mat;
+	}
 }
