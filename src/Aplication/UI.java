f7063fffd411b9abd5539d9a601532a4503be921
MessiasFilho <messiasfilho98@gmail.com>
2022-04-25 16:57:17 -0300

    Update UI.java

--- src/Aplication/UI.java
+++ src/Aplication/UI.java
@@ -32,7 +32,13 @@ public class UI {
 		public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 		public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
-	
+		// clear scream 
+		public static void ClearScream () {
+			System.out.print("\033[H\033[2J");
+			System.out.flush();
+		}
+		
+		
 		public static ChessPosition ReadChessPosition (Scanner sc ) {
 			try {
 				String s = sc.nextLine(); 
